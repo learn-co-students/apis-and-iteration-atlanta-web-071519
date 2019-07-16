f0315084ac2b0c0e51364db07fea03f330c88b68
John Odom <john.osborne.odom@gmail.com>
2019-07-16 17:55:54 -0400

    Star Wars Character CLI Film Returner

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,27 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  titles_hash = []
 
+  response_hash["results"].each_with_index do |descriptors,index|
+    
+    descriptors.each do |attribute, answer_to_descriptors|
+      
+      if attribute == "name"
+        downcased_character = answer_to_descriptors.downcase
+      end
+
+      if character_name == downcased_character
+        response_hash["results"][index]["films"].each do |url|
+          response = RestClient.get(url)
+          titles_hash.push(JSON.parse(response.body)["title"])
+        end
+      end
+    end
+  end
+  titles_hash
+
+  
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -20,6 +40,9 @@ end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |index|
+    puts index
+  end
 end
 
 def show_character_movies(character)
